a07af2f626545b986cdd540f3c61fc4503c3dfd8
Kabuto <591775790@qq.com>
2019-10-08 18:03:16 +0800

    10.08 webpack核心概念

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,5 +1,28 @@
-var Header = require('./header.js');
-var Content = require('./content.js');
+// import Header from './header.js'
+// import Content from './content.js'
+// import testimg from './testimg.png'
+// import imgstyle from './root.css'
+// import setClass from './fun.js'
 
-new Header();
-new Content();
+// setClass()
+
+// var img = new Image()
+// img.src = testimg
+// img.classList.add(imgstyle.imgstyle)
+
+// var root = document.getElementById('root')
+// root.append(img)
+
+// new Header();
+// new Content();
+
+// console.log('bb')
+import './item.css'
+var btn = document.createElement('button')
+btn.innerHTML = '新增'
+document.body.appendChild(btn)
+btn.onclick = function () {
+  var div = document.createElement('div')
+  div.innerHTML = 'item'
+  document.body.append(div)
+}
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,8 +1,48 @@
 const path = require('path');
+const webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    hotOnly: true
+  },
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'imgs/'
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          }, 'postcss-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
